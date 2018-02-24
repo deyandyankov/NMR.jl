479df598442f7d60d6dbd1005b8caf4c771b869a
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-02-24 16:02:19 +0200

    Test for List of Flights objective

---
+++ test/test_listofflights.jl
@@ -0,0 +1,6 @@
+### Create a list of flights based on the Flight id, this output should include the passenger Id,
+# relevant IATA/FAA codes, the departure time, the arrival time
+# (times to be converted toHH:MM:SS format), and the flight times.
+# NMR.runjob(NMR.Nmr(2, ["AComp_Passenger_data.csv"], NMR.mapper_parserecordacomp, "acompdata.json"))
+# c = NMR.runcombiner("acompdata.json", NMR.combiner_parsejson)
+# @show c

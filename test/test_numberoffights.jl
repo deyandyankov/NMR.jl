dd944c240d7ce0bea61fbc803c9b6834821fef99
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-02-24 15:59:42 +0200

    Test for number of passengers

--- test/test_numberoffights.jl
+++ test/test_numberoffights.jl
@@ -1,11 +1,5 @@
 ### Determine the number of flights from each airport, include a list of any airports not used.
-NMR.runjob(NMR.Nmr(1, ["AComp_Passenger_data.csv"], NMR.mapper_parserecordacomp, "acomp.csv"))
-NMR.runjob(NMR.Nmr(1, ["acomp.csv"], NMR.reducer_numberofflights, "acomp_flights_reduced.csv"))
-
-c = NMR.runcombiner("acomp_flights_reduced.csv", NMR.combiner_parsejson)
-@show c
-#
-# @test typeof(c) == Vector{String}
-# @test length(c) == 17
-# @test JSON.parse(c[1])[1] == "AMS"
-# @test JSON.parse(c[1])[2] == 13
+# NMR.runjob(NMR.Nmr(1, ["AComp_Passenger_data.csv"], NMR.mapper_parserecordacomp, "acomp.csv"))
+# NMR.runjob(NMR.Nmr(1, ["acomp.csv"], NMR.reducer_numberofflights, "acomp_flights_reduced.csv"))
+# c = NMR.runcombiner("acomp_flights_reduced.csv", NMR.combiner_parsejson)
+# @show c
---
+++ test/test_numpassengers.jl
@@ -0,0 +1,6 @@
+### Calculate the number of passengers on each flight
+# NMR.runjob(NMR.Nmr(3, ["AComp_Passenger_data.csv"], NMR.mapper_parserecordacomp, "acomp.csv"))
+# NMR.runjob(NMR.Nmr(3, ["acomp.csv"], NMR.reducer_numberofpassengers, "acomp_reduced.json"))
+# c = NMR.runcombiner("acomp_reduced.json", NMR.combiner_parsejson)
+#
+# @show c

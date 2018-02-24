e0cadabb9862d2763ff4d5261f357b106d6d6bb3
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-02-24 15:30:19 +0200

    Handle cases where the time of flight is not an integer

--- src/udf/types.jl
+++ src/udf/types.jl
@@ -16,30 +16,32 @@ end
 
 function UDFAirportCode{T<:AbstractString}(value::T)
   m = match(r"^[A-Z]{3}$", value)
-  is(typeof(m), Void) && throw(UDFException("Airport id must be comprised of three capital letters. Trying to initialise with $value instead."))
+  typeof(m) === Void && throw(UDFException("Airport id must be comprised of three capital letters. Trying to initialise with $value instead."))
   value
 end
 
 function UDFFlightId{T<:AbstractString}(value::T)
   m = match(r"^[A-Z0-9]{8}$", value)
-  is(typeof(m), Void) && throw(UDFException("Invalid flight id: $value"))
+  typeof(m) === Void && throw(UDFException("Invalid flight id: $value"))
   value
 end
 
 function UDFPassengerId{T<:AbstractString}(value::T)
   m = match(r"^[A-Z0-9]{10}$", value)
-  is(typeof(m), Void) && throw(UDFException("Invalid passenger id: $value"))
+  typeof(m) === Void && throw(UDFException("Invalid passenger id: $value"))
   value
 end
 
 UDFDepartureTime{T<:Real}(value::T) = Dates.unix2datetime(value)
 function UDFDepartureTime{T<:AbstractString}(value::T)
   m = match(r"^[0-9]*$", value)
-  is(typeof(m), Void) && throw(UDFException("Invalid DepartureTime: $value"))
+  typeof(m) === Void && throw(UDFException("Invalid DepartureTime: $value"))
   UDFDepartureTime(parse(value))
 end
 
 function UDFTotalFlightTime{T<:AbstractString}(value::T)
+  m = match(r"^[0-9]+$", value)
+  typeof(m) === Void && throw(UDFException("Invalid flight time, cannot parse to integer: $value"))
   x = parse(Int16, value)
   x < 0 && throw(UDFException("total flight time must be a positive integer"))
   x > 1200 && throw(UDFException("Invalid flight time $x - flights cannot go longer than 20 hours"))

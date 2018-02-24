91e7ea7f8b4df4de032e87609b82f30e38721fc0
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-02-24 14:16:41 +0200

    Add main execution point for the package and several data-related environment variables.

--- src/NMR.jl
+++ src/NMR.jl
@@ -1,5 +1,18 @@
-module NMR
+"""
+nmr - Naive MapReduce module
 
-# package code goes here
+NMR is a Naive MapReduce implementation.
+It has a simple interface that allows the user to input a filename and process it in a map-reduce fashion.
+"""
+module NMR
+  using JSON
+
+  pkgdir = Pkg.dir("NMR")
+  datadir = "NMR_DATADIR" in keys(ENV) ? ENV["NMR_DATADIR"] : joinpath(pkgdir, "test", "data")
+  splitdir = "NMR_SPLITDIR" in keys(ENV) ? ENV["NMR_SPLITDIR"] : joinpath(pkgdir, "test", "split")
+  outputdir = "NMR_OUTPUTDIR" in keys(ENV) ? ENV["NMR_OUTPUTDIR"] : joinpath(pkgdir, "test", "output")
+
+  ctx = Dict{String, Any}()
+  default_ctx = Dict{String, Any}()
 
 end # module
